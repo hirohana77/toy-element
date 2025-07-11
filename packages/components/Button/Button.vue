0f7a35a8df61c9173a79b53e2ca3445fae33aee1
刘广华 <14100763+hirohana@user.noreply.gitee.com>
2025-07-11 14:43:42 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineOptions({
+    name: 'ErButton',
+});
+
+</script>
+
+<template>
+    <button style="background-color: blue; color: red;"> this a button</button>
+</template>
